1f5b378a169198696334913202a577f5e0c2423e
andrii-pozniak <andriipozniak@gmail.com>
2023-05-08 21:03:28 +0300

    1

--- controllers/signup.js
+++ controllers/signup.js
@@ -59,7 +59,7 @@ const login = async (req, res, next) => {
       id: user._id,
     };
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "10h" });
-    await User.findByIdAndUpdate(user._id, { token }, user.name);
+    await User.findByIdAndUpdate(user._id, { token });
     return res.status(200).json({ token }, user.name);
   } catch (error) {
     next(error);
